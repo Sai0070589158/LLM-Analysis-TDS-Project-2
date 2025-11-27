839cab8e8afebda356706e338efb16872a9ea0f2
Sai0070589158 <sainathshelke06@gmail.com>
2025-11-27 13:50:07 +0530

    Added Dockerfile

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ async def solve(request: Request, background_tasks: BackgroundTasks):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
